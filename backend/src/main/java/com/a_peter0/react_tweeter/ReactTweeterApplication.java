3b830821e31e96c116451e5ad2107c4e1e0eda3c
Peter Zelinskiy <wiindon.mail@gmail.com>
2021-01-19 20:14:44 +0300

    init

---
+++ backend/src/main/java/com/a_peter0/react_tweeter/ReactTweeterApplication.java
@@ -0,0 +1,13 @@
+package com.a_peter0.react_tweeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactTweeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactTweeterApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/a_peter0/react_tweeter/ReactTweeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a_peter0.react_tweeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactTweeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
